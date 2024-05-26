4820f367fa34818ff0bd76826f041c75aa6a7096
fifi <clifford.juan@engineer.com>
2024-05-26 23:09:29 +0800

    Create ad_ds_deployment.ps1

---
+++ PowerShell/05_windows_server/ad_ds_deployment.ps1
@@ -0,0 +1,17 @@
+#
+# Windows PowerShell script for AD DS Deployment
+#
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "Corp.MyDomain.com" `
+-DomainNetbiosName "CORP" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
