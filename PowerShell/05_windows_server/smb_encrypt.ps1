60a927d4eb49248fce3394f7a8960d61a45c26f6
fifi <clifford.juan@engineer.com>
2024-08-11 19:20:15 +0800

    Update smb_encrypt.ps1
    Added an item and the reference.

--- PowerShell/05_windows_server/smb_encrypt.ps1
+++ PowerShell/05_windows_server/smb_encrypt.ps1
@@ -5,3 +5,9 @@ Set-SmbShare –Name <sharename> -EncryptData $true
 
 # Encrypt all shares on a file server
 Set-SmbServerConfiguration –EncryptData $true
+
+# Create a new SMB file share on a server and enable SMB encryption at the same time
+New-SmbShare –Name <sharename> -Path <pathname> –EncryptData $true
+
+# Reference
+# https://learn.microsoft.com/en-us/training/modules/harden-windows-server/6-secure-smb-traffic
