618cf4eb3f7296c0f074a093fc1bcc47b1851a83
fifi <clifford.juan@engineer.com>
2024-08-22 23:15:33 +0800

    Update locate_inactive_user.ps1
    Add disabling the account from the result set.

--- PowerShell/08_active_directory/secure_user_accounts/locate_inactive_user.ps1
+++ PowerShell/08_active_directory/secure_user_accounts/locate_inactive_user.ps1
@@ -1,3 +1,6 @@
 # Find users that have not signed in within the last n days
 $n = 90
 Get-ADUser -Filter {LastLogonTimeStamp -lt (Get-Date).Adddays(-($n))-and enabled -eq $true} -Properties LastLogonTimeStamp
+
+# Disable the account using the result set
+# Get-ADUser -Filter {LastLogonTimeStamp -lt (Get-Date).Adddays(-($n))-and enabled -eq $true} -Properties LastLogonTimeStamp | Disable-ADAccount
