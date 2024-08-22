f5c31b657cda71856def1cf9f465955523915a37
fifi <clifford.juan@engineer.com>
2024-08-22 23:10:02 +0800

    Update and rename locate_user.ps1 to locate_inactive_user.ps1
    Remove script locating non-expiring users. Created new file for it in -PowerShell/08_active_directory/secure_user_accounts/enable_password_expire.ps1

--- PowerShell/08_active_directory/secure_user_accounts/locate_inactive_user.ps1
+++ PowerShell/08_active_directory/secure_user_accounts/locate_inactive_user.ps1
@@ -1,8 +1,3 @@
-# Locate problematic accounts
-
-# Find active users with passwords set to never expire
-Get-ADUser -Filter {Enabled -eq $true -and PasswordNeverExpires -eq $true}
-
 # Find users that have not signed in within the last n days
 $n = 90
 Get-ADUser -Filter {LastLogonTimeStamp -lt (Get-Date).Adddays(-($n))-and enabled -eq $true} -Properties LastLogonTimeStamp
