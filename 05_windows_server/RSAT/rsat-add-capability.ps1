7dac89a78be5de310145457ef762e3767059b41d
fifi <clifford.juan@engineer.com>
2026-03-29 05:01:00 +0800

    Add rsat-add-capability.ps1 for RSAT tool installation
    Script to install a specific RSAT tool by capability name.

---
+++ 05_windows_server/RSAT/rsat-add-capability.ps1
@@ -0,0 +1,4 @@
+# Install a specific RSAT tool by Capability [Name](https://raw.githubusercontent.com/cliffvj/Windows-Azure-Scripts/refs/heads/main/05_windows_server/RSAT/rsat_get-capability2.ps1)
+
+# Add-WindowsCapability -Online -Name "Rsat.NetworkController.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "<tool_name>"
